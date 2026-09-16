323acfbdd13c164c65c3eaaf7ba3a4988acf2c3c
lbvrrhh389-cmyk <lbvrrhh389@gmail.com>
2026-09-16 20:02:33 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+
+/**
+ * LBV RRHH - Configuración Firebase
+ * Debe definir window.FIREBASE_CONFIG para que script.js lo use.
+ */
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyB2ZtAXrFWVEeMuCVdHuDqKI2vnh2a8K18",
   authDomain: "lbv-rrhh.firebaseapp.com",
   projectId: "lbv-rrhh",
@@ -8,3 +12,6 @@ const firebaseConfig = {
   appId: "1:239071862361:web:355154dbe23effbc579b48",
   measurementId: "G-N6PQ1GLC7P"
 };
+
+// Activa Firebase cuando la config está completa
+window.FIREBASE_ENABLED = true;

db5812d6b42092619eefb33ceceefaa26fd4b32f
lbvrrhh389-cmyk <lbvrrhh389@gmail.com>
2026-09-17 10:55:59 -0300

    Update google-config.js

--- google-config.js
+++ google-config.js
@@ -7,6 +7,6 @@
  */
 window.GOOGLE_SYNC_CONFIG = {
   // Ejemplo: https://script.google.com/macros/s/XXXX/exec
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwrcAm1qo65i1ajGlf13CIucXI6Gv5FAaENjqfpwKxNaDKwmrec_RJL9ywkt4x5JC9BaA/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw-0s_3m3DS73NrgESPG8RgnnFp8tbdBupOlxmEv6jGNX7dgqlE0ilEuI0yU_43IHi4kw/exec',
   ENABLED:true // poné true cuando pegues la URL
 };
